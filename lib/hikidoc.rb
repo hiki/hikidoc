a58cd7097a49cfd07e1c1e052f685892f8f3b1f5
kazuhiko <kazuhiko@a4544477-fcff-0310-b425-9d56a9aeec48>
2005-12-28 23:37:11 +0000

    more fixes to support relative inline URL
    git-svn-id: http://hikidoc.rubyforge.org/svn/trunk@36 a4544477-fcff-0310-b425-9d56a9aeec48

--- lib/hikidoc.rb
+++ lib/hikidoc.rb
@@ -347,7 +347,7 @@ class HikiDoc < String
       else
         uri = title = link
       end
-      uri.sub!( /^\w+:/, '' ) if %r|://| !~ uri && /^mailto:/ !~ uri
+      uri.sub!( /^(?:https?|ftp|file)+:/, '' ) if %r|://| !~ uri && /^mailto:/ !~ uri
       store_block( %Q|<a href="#{escape_quote( uri )}">#{title}</a>| )
     end
     ret.gsub!( URI_RE ) do |uri|
