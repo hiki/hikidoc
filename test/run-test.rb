550c0d0fcebe6694deb64e1bbe1bbde406086b23
kou <kou@a4544477-fcff-0310-b425-9d56a9aeec48>
2007-12-31 09:33:54 +0000

    * added test runner.
    git-svn-id: http://hikidoc.rubyforge.org/svn/trunk@81 a4544477-fcff-0310-b425-9d56a9aeec48

---
+++ test/run-test.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require "test/unit"
+
+if Test::Unit::AutoRunner.respond_to?(:standalone?)
+  exit Test::Unit::AutoRunner.run($0, File.dirname($0))
+else
+  exit Test::Unit::AutoRunner.run(false, File.dirname($0))
+end
