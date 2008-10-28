63cf92b41238dd8fe420001fd179cb8142c56c73
kazuhiko <kazuhiko@a4544477-fcff-0310-b425-9d56a9aeec48>
2008-10-28 12:27:05 +0000

    modify the runner script to support both ruby-1.9 and ruby-1.8.
    git-svn-id: http://hikidoc.rubyforge.org/svn/trunk@108 a4544477-fcff-0310-b425-9d56a9aeec48

--- test/run-test.rb
+++ test/run-test.rb
@@ -2,8 +2,12 @@
 
 require "test/unit"
 
-if Test::Unit::AutoRunner.respond_to?(:standalone?)
-  exit Test::Unit::AutoRunner.run($0, File.dirname($0))
+if Test::Unit.respond_to?(:setup_argv)
+  Test::Unit::setup_argv{[File.dirname($0)]}
 else
-  exit Test::Unit::AutoRunner.run(false, File.dirname($0))
+  if Test::Unit::AutoRunner.respond_to?(:standalone?)
+    exit Test::Unit::AutoRunner.run($0, File.dirname($0))
+  else
+    exit Test::Unit::AutoRunner.run(false, File.dirname($0))
+  end
 end
